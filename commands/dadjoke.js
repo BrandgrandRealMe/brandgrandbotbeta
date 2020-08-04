eefac8eba2f4f9e687b88e95fb4a400e2e17b6b7
BrandgrandReal <brandgrandbusiness@gmail.com>
2020-08-04 17:29:52 +0000

    Updated Joke To Work! Mad It A Embed

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -6,7 +6,13 @@ module.exports = {
   usage: 'dadjoke',
   execute: async (message, args, client, db, packageInfo, Discord) => {
     args = args.join(" ");
-    message.channel.send(`${dadjoke[Math.floor(Math.random() * dadjoke.length)]}`);
+    joke = dadjoke.jokes[Math.floor(Math.random() * dadjoke.jokes.length)];
+      const embed = new Discord.MessageEmbed()
+  .setColor(0xFFFF00)
+  .addField('DADJOKE!', joke)
+  .setFooter(`© Brandgrand!bot Made by Brandgrand!real`);
+  message.delete().catch(O_o=>{}); 
+  message.channel.send({embed});
         }
     
 };
